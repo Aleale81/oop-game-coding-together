c49a13759d9c8775d12597f73c882eb69a4178f5
Alessandra Scarpellini <scaalessandra@icloud.com>
2022-09-15 17:15:56 +0200

    add css and interva to create new obstacles

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 class Game {
     constructor(){
         this.player = null; // will store an instance of the class Player
+        this.obstacles = [];  // will store instances of a class
     }
     start(){
         console.log('starting game....')
         this.player = new Player(); 
         this.attachEventListeners();
+
+        this.obstacle = new Obstacle;
+//move obstacles
+        setInterval(() => {
+        this.obstacles.forEach((obstacleInstance) => {
+        obstacleInstance.moveDown()
+        });
+        }, 60);
+//create new obstacles
+        setInterval(() => {
+        const newObstacle = new Obstacle();
+        this.obstacles.push(newObstacle)
+        },3000)
+        
+
     }
     attachEventListeners(){
         document.addEventListener("keydown", (event) => {
@@ -53,10 +69,36 @@ class Player {
         this.domElement.style.left = this.positionX + "vw";
 
     }
+}
+class Obstacle {
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 90;
+        this.width = 10;
+        this.height = 10;
+        this.domElement = null   
+        
+        this.createDomElement();
+    }
+    moveDown(){
+        this.positionY--
+        this.domElement.style.bottom = this.positionY +"vh"
+    }
+    createDomElement(){
+        // create dom element
+        this.domElement = document.createElement('div');
 
+        // set id and css
+        this.domElement.className = "obstacle";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.bottom = this.positionY + "vh";
+        this.domElement.style.left = this.positionX + "vw";
 
-
-
+        // append to the dom
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElement)
+    }
 }
 
 const game = new Game();
